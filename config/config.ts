893c93df7a5df1a50ae3870a2c790730ef8ccfec
guangzan <guangzan1999@outlook.com>
2022-06-16 09:54:47 +0800

    chore: update config

--- config/config.ts
+++ config/config.ts
@@ -2,14 +2,10 @@ import { defineConfig } from 'umi';
 import { routes } from './router';
 import { theme } from './theme';
 
-// const __DEV__ = process.env.NODE_ENV === 'development';
-
 export default defineConfig({
   theme,
   routes,
   title: 'Lab',
-  // base: __DEV__ ? '/' : '/front-end-laboratory/',
-  // publicPath: __DEV__ ? '/' : '/front-end-laboratory/',
   outputPath: 'dist',
   dynamicImport: {
     loading: '@/components/Loading',
